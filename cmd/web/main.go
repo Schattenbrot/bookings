fc11b86302c689702163b99e7fab804e8d944a67
Schattenbrot <aaron.machill@web.de>
2021-12-20 00:29:09 +0100

    Added form-handling and serverside validation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/Schattenbrot/bookings/internal/config"
 	"github.com/Schattenbrot/bookings/internal/handlers"
+	"github.com/Schattenbrot/bookings/internal/models"
 	"github.com/Schattenbrot/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -19,6 +21,8 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
 
 	// change this to true when in production
 	app.InProduction = false
